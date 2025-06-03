0697bbb3122a881c9db57e2190ea79ac2dd4d40f
gitMorelli <andrea.morels14@gmail.com>
2025-06-04 01:44:35 +0200

    new tests

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -68,6 +68,7 @@ if __name__ == "__main__":
     exclusion = [1,  2,  3,  7,  8,  9, 10, 16, 22, 25, 26, 28, 31, 32, 34, 37,
        38, 40, 43, 44, 46, 52, 55, 56, 57, 58, 61, 62, 63, 64, 67, 68, 69,
        70, 73, 74, 76, 79, 80]
+    exclusion=[]
 
     try_args, total_jobs = generate_experiments(config_path, log_file)
     print(f"Total experiments to run: {total_jobs}")
